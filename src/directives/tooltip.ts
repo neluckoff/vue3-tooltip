b1ed4f91b7b26e519427e03fad12c77fe1c065d2
Dmitry Luckyanenko <neluckoff@gmail.com>
2024-03-11 17:52:19 +0300

    [1.2.19] Added reactivity v-tooltipe

--- src/directives/tooltip.ts
+++ src/directives/tooltip.ts
@@ -3,13 +3,22 @@ import '../assets/directive.css';
 
 const TooltipDirective: Directive<HTMLElement, string> = {
   mounted: (el, binding) => {
-    const className = 'vue-tooltip';
-    el.dataset.tooltip = binding.value;
-    el.classList.add(className);
-    el.classList.add(`${className}-data`);
-    el.classList.toggle(`${className}__${getStyleClass(binding.modifiers)}`, true);
-    el.classList.toggle(`${className}__${binding.arg || 'bottom'}`, true);
+    updateTooltip(el, binding);
+  },
+  updated: (el, binding) => {
+    updateTooltip(el, binding);
   }
+};
+
+const updateTooltip = (el: HTMLElement, binding: { value: string; modifiers: { [x: string]: boolean; }; arg?: string; }) => {
+  const className = 'vue-tooltip';
+  el.classList.add(className);
+  if (binding.value === null || binding.value === undefined)
+    return;
+  el.dataset.tooltip = binding.value;
+  el.classList.add(`${className}-data`);
+  el.classList.toggle(`${className}__${getStyleClass(binding.modifiers)}`, true);
+  el.classList.toggle(`${className}__${binding.arg || 'bottom'}`, true);
 };
 
 const getStyleClass = (modifiers: { [x: string]: boolean; }) => {
@@ -19,4 +28,4 @@ const getStyleClass = (modifiers: { [x: string]: boolean; }) => {
   return 'primary'; 
 };
 
-export default TooltipDirective
+export default TooltipDirective;
